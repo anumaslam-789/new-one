56262e52e531fb3f9f9b8a5ac00b4584814a19c7
anumaslam-789 <130720606+anumaslam-789@users.noreply.github.com>
2023-09-24 01:31:13 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAG6SkSGtfPBPOSgdPmDN6z08JYAfoDFow",
+  authDomain: "login-form-b3c04.firebaseapp.com",
+  projectId: "login-form-b3c04",
+  storageBucket: "login-form-b3c04.appspot.com",
+  messagingSenderId: "428430991061",
+  appId: "1:428430991061:web:2602b6d0fe08cf9f2d8c5c",
+  measurementId: "G-TQN6NBF0N4"
+}
+
+
+const app = initializeApp(firebaseConfig);
